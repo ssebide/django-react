dc679faddbbf9ab50f2dfe950a7ae149850a1246
ssebide <sebiddejohn136@gmail.com>
2025-01-15 00:41:23 +0300

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl =
+  "/https://3be4cda6-79f4-484b-a7db-4671b985a284-dev.e1-us-east-azure.choreoapis.dev/djangoreact/backend/v1";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
